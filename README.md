c084f3892e0173087fafd37c43d876719b56766d
Maxgroene <maxg@tuta.com>
2024-05-22 12:28:43 +0200

    adding text to readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # WInShellAppInstaller
+
+This repository contains a set of scripts to facilitate the installation of applications on Windows systems. 
+
+## Usage
+
+1. **Allow Script Execution:**
+   
+   Before running the installation script, ensure that PowerShell script execution is allowed. You can do this by executing the following command:
+
+   ```powershell
+   Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+2. **Run Scrypt:**
+    ```powershell
+    .\install-apps.ps1
+
+3. **Deny Script Execution:**
+   
+   after running the installation script, ensure that PowerShell script execution is denyded. You can do this by executing the following command:
+
+   ```powershell
+   Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser
--- install-apps.ps1
+++ install-apps.ps1
@@ -19,44 +19,46 @@ $apps = @(
     "Valve.Steam",
     "WinSCP.WinSCP",
     "angryziber.AngryIPScanner",
+    "Tutanota.Tutanota",
+    "ApacheFriends.Xampp.8.2",
     "9WZDNCRD29V9"
 )
 Write-Host "
-
            _____  _____      _____     _        _ _           
      /\   |  __ \|  __ \    |_   _|   | |      | | |          
     /  \  | |__) | |__) |_____| |  ___| |_ __ _| | | ___ _ __ 
    / /\ \ |  ___/|  ___/______| | / __| __/ _` | | |/ _ \ '__|
   / ____ \| |    | |         _| |_\__ \ || (_| | | |  __/ |   
  /_/    \_\_|    |_|        |_____|___/\__\__,_|_|_|\___|_|   
-                                                              
-                                                              
-
 " -ForegroundColor Green
-Write-Host "From Max Groeneweg" -ForegroundColor Red
+Write-Host "From Max Groeneweg" -ForegroundColor Blue
 
-$installApps = Read-Host "Moechten Sie die Apps installieren? (ja/nein)"
+$installApps = Read-Host "Do you want to Install the Apps? (y/n)"
 
-$customizeTerminal = Read-Host "Moechten Sie das Terminal grafisch anpassen? (ja/nein)"
+$customizeTerminal = Read-Host "Do you want to changing the colors of your terminal? (y/n)"
 
-if ($installApps -eq "ja") {
+if ($installApps -eq "y") {
     foreach ($app in $apps) {
-        Write-Host "Installiere $app..."
+        Write-Host "Installing $app..."
         winget install --id $app --silent --accept-package-agreements --accept-source-agreements
         if ($LASTEXITCODE -eq 0) {
-            Write-Host "$app wurde erfolgreich installiert."
+            Write-Host "$app Installed successfully."
         } else {
-            Write-Host "Fehler bei der Installation von $app."
+            Write-Host "error installing $app."
         }
     }
 } else {
-    Write-Host "App-Installation Uebersprungen."
+    Write-Host "App Installation skipped."
 }
 
-if ($customizeTerminal -eq "ja") {
-    Write-Host "Passe das Terminal grafisch an..."
-    Write-Host "Terminal wurde angepasst."
+if ($customizeTerminal -eq "y") {
+    Write-Host "Changing the colors of the terminal..."
+    winget install JanDeDobbeleer.OhMyPosh --silent --accept-package-agreements --accept-source-agreements
+    New-Item -Type File -Force $profile
+    Add-Content -Path $PROFILE -Value 'oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/jandedobbeleer.omp.json | Invoke-Expression'
+    Write-Host "The colors of the Terminal changed succesfully."
 } else {
-    Write-Host "Grafische Anpassung des Terminals Uebersprungen."
+    Write-Host "Changing colors of the Terminal was skipped."
 }
+
 
