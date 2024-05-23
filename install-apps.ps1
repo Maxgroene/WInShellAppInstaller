413f5afd6723db40027039f3220b429dcd215654
Maxgroene <maxg@tuta.com>
2024-05-23 08:56:27 +0200

    adding arduino drivers

--- install-apps.ps1
+++ install-apps.ps1
@@ -21,21 +21,28 @@ $apps = @(
     "angryziber.AngryIPScanner",
     "Tutanota.Tutanota",
     "ApacheFriends.Xampp.8.2",
+    "valinet.ExplorerPatcher",
     "9WZDNCRD29V9"
 )
 Write-Host "
-           _____  _____      _____     _        _ _           
-     /\   |  __ \|  __ \    |_   _|   | |      | | |          
-    /  \  | |__) | |__) |_____| |  ___| |_ __ _| | | ___ _ __ 
-   / /\ \ |  ___/|  ___/______| | / __| __/ _` | | |/ _ \ '__|
-  / ____ \| |    | |         _| |_\__ \ || (_| | | |  __/ |   
- /_/    \_\_|    |_|        |_____|___/\__\__,_|_|_|\___|_|   
+                           _____           _        _ _           
+     /\                   |_   _|         | |      | | |          
+    /  \   _ __  _ __ ______| |  _ __  ___| |_ __ _| | | ___ _ __ 
+   / /\ \ | '_ \| '_ \______| | | '_ \/ __| __/ _` | | |/ _ \ '__|
+  / ____ \| |_) | |_) |    _| |_| | | \__ \ || (_| | | |  __/ |   
+ /_/    \_\ .__/| .__/    |_____|_| |_|___/\__\__,_|_|_|\___|_|   
+          | |   | |                                               
+          |_|   |_|                                               
 " -ForegroundColor Green
 Write-Host "From Max Groeneweg" -ForegroundColor Blue
 
 $installApps = Read-Host "Do you want to Install the Apps? (y/n)"
 
 $customizeTerminal = Read-Host "Do you want to changing the colors of your terminal? (y/n)"
+
+$WindowsDarkMode = Read-Host "Do you want to enable the windows Dark Mode? (y/n)"
+
+$InstallCP210xDriver = Read-Host "Do you want to Install the CP210x Driver? (y/n)"
 
 if ($installApps -eq "y") {
     foreach ($app in $apps) {
@@ -55,10 +62,53 @@ if ($customizeTerminal -eq "y") {
     Write-Host "Changing the colors of the terminal..."
     winget install JanDeDobbeleer.OhMyPosh --silent --accept-package-agreements --accept-source-agreements
     New-Item -Type File -Force $profile
-    Add-Content -Path $PROFILE -Value 'oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/jandedobbeleer.omp.json | Invoke-Expression'
+    Add-Content -Path $PROFILE -Value 'oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/powerline.omp.json | Invoke-Expression'
     Write-Host "The colors of the Terminal changed succesfully."
 } else {
     Write-Host "Changing colors of the Terminal was skipped."
+}
+
+if ($WindowsDarkMode -eq "y") {
+    $personalizePath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+    $appsUseLightTheme = "AppsUseLightTheme"
+    $systemUsesLightTheme = "SystemUsesLightTheme"
+
+    # Set both themes (Apps and System) to dark mode
+    Set-ItemProperty -Path $personalizePath -Name $appsUseLightTheme -Value 0
+    Set-ItemProperty -Path $personalizePath -Name $systemUsesLightTheme -Value 0
+    
+    Write-Host "Windows DarkMode enabled."
+} else {
+    Write-Host "Changing To Windows DarkMode was skipped."
+}
+
+if ($InstallCP210xDriver -eq "y") {
+# Definiere die URLs und Pfade
+$downloadUrl = "https://www.silabs.com/documents/public/software/CP210x_Universal_Windows_Driver.zip"
+$zipFilePath = "$env:TEMP\CP210x_Universal_Windows_Driver.zip"
+$extractPath = "$env:TEMP\CP210x_Universal_Windows_Driver"
+
+# Herunterladen der ZIP-Datei
+Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath
+
+# Erstellen des Verzeichnisses für die entpackten Dateien
+if (!(Test-Path -Path $extractPath)) {
+    New-Item -ItemType Directory -Path $extractPath | Out-Null
+}
+
+# Entpacken der ZIP-Datei
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipFilePath, $extractPath)
+
+# Installieren des Treibers
+$infPath = Get-ChildItem -Path $extractPath -Filter "*.inf" -Recurse | Select-Object -First 1
+pnputil.exe /add-driver $infPath.FullName /install
+
+# Bereinigen der temporären Dateien
+Remove-Item -Path $zipFilePath -Force
+Remove-Item -Path $extractPath -Recurse -Force
+
+Write-Host "Treiberinstallation abgeschlossen."
 }
 
 
